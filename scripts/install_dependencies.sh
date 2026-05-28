58cd8b70533c764c291af56ed108a6319ed0fd1c
Krishna Kumar <krishnasgk003@gmail.com>
2026-05-28 08:02:21 +0000

    Initial microservice commit

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /home/ubuntu/app
+
+sudo apt update
+sudo apt install nodejs npm -y
+
+npm install
---
+++ scripts/start_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/ubuntu/app
+
+nohup node app.js > output.log 2>&1 &
